0f9e27357490777ae53cff0e75fad1df43a1cfc7
Felipe Saavedra <fisaavedrae@gmail.com>
2024-03-28 22:58:29 -0300

    cuarta

--- backend/database/index.js
+++ backend/database/index.js
@@ -1,13 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-/*
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
-
 });
-*/
 
+/*
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
@@ -16,5 +14,6 @@ const pool = new Pool({
   port: process.env.DB_PORT,
   allowExitOnIdle: true,
 });
+*/
 
 module.exports = pool;
